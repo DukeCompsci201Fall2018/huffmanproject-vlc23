e27adb0a9e7efa9c20c418cacfb5f384eb3fd8b8
Virginia Capehart <virginia.capehart@duke.edu>
2018-12-05 20:05:39 -0500

    null pointer in decompress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -35,10 +35,8 @@ public class HuffProcessor {
 	/**
 	 * Compresses a file. Process must be reversible and loss-less.
 	 *
-	 * @param in
-	 *            Buffered bit stream of the file to be compressed.
-	 * @param out
-	 *            Buffered bit stream writing to the output file.
+	 * @param in - Buffered bit stream of the file to be compressed.
+	 * @param out - Buffered bit stream writing to the output file.
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
 
@@ -52,17 +50,61 @@ public class HuffProcessor {
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
 	 *
-	 * @param in
-	 *            Buffered bit stream of the file to be decompressed.
-	 * @param out
-	 *            Buffered bit stream writing to the output file.
+	 * @param in - Buffered bit stream of the file to be decompressed.
+	 * @param out - Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		
+		int bits = in.readBits(BITS_PER_INT);
+		if(bits != HUFF_TREE) {
+			throw new HuffException("illegal header starts with " + bits);
+		}
+		
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
+		out.close();
+	}
+	
+	public HuffNode readTreeHeader(BitInputStream in) {
+		int bit = in.readBits(BITS_PER_INT);
+		if(bit == -1) {
+			throw new HuffException("reading bits failed");
+		}
+		if(bit == 0) { //an internal node --> recursion
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0, 0, left, right);
+		}
+		else {
+			int val = in.readBits(BITS_PER_WORD + 1);
+			return new HuffNode(val, 0, null, null);
 		}
 	}
+	
+	public void readCompressedBits(HuffNode node, BitInputStream in, BitOutputStream out) {
+		HuffNode current = node;
+		while(true) {
+			int bits = in.readBits(1);
+			if(bits == -1) 
+				throw new HuffException("bad input");
+			else {
+				if(bits == 0) {
+					current = current.myLeft; //read a 0, go left 
+				}
+				else {
+					if(current.myLeft == null && current.myRight == null) { //at leaf
+						if(current.myValue == PSEUDO_EOF) {
+							break; //end of file
+						}
+						else {
+							out.writeBits(1, current.myValue);
+							current = node; //start again
+						}
+					}
+				}
+			}
+		}
+	}
+	
 }
+
